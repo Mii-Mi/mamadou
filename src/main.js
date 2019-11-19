a4bcda2e4789a6d612cf4aca2667bab79d276b25
Cécile <peigne.cecile@free.fr>
2019-11-20 00:17:01 +0100

    vue router ok

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import router from './router/index'
 
 Vue.config.productionTip = false
 
+Vue.use(VueAxios, axios)
+
 new Vue({
   vuetify,
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app')
+
