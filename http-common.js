eb56b9ed6ffd919845453743305400e40e6bbb01
Cécile <peigne.cecile@free.fr>
2019-11-25 17:28:47 +0100

    change localhost in dns

--- http-common.js
+++ http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
  
 export default axios.create({
-  baseURL: "http://0.0.0.0:3333/",
+  baseURL: "https://cecile.cf:3333/",
   headers: {
     "Content-type": "application/json",
   },
