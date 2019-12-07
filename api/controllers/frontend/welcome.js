80f51115b9fc1a440ae95c26ad0c18ed83c3cb5b
Cécile <peigne.cecile@free.fr>
2019-12-07 10:36:11 +0100

    add ORDER to front titles request

--- api/controllers/frontend/welcome.js
+++ api/controllers/frontend/welcome.js
@@ -2,7 +2,7 @@ const pool = require('../../db/index')
 
 module.exports = (req, res, next) => {
 
-  pool.query('SELECT * FROM titles', (err, titles) => {
+  pool.query('SELECT * FROM titles ORDER BY position ASC', (err, titles) => {
     if (err) return next(err)
     res.json(titles.rows)
   })
